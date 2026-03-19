6dccaf33f32d621ce9036fd7a3635aa8dbf41996
4a01004 <mangi2703@gmail.com>
2026-03-19 10:24:23 +0900

    docs:interface, msg_ws, bridge_ws를 한번에 빌드가능하게 스크립트 수정

--- bridge_install_script.sh
+++ bridge_install_script.sh
@@ -1,4 +1,4 @@
-BASE_DIR="$(pwd)"
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$BASE_DIR/msg_ws"
 colcon build --symlink-install
 
@@ -11,4 +11,14 @@ colcon build \
   --cmake-args \
   -Dautoware_auto_vehicle_msgs_DIR="$BASE_DIR/msg_ws/install/autoware_auto_vehicle_msgs/share/autoware_auto_vehicle_msgs/cmake"
 
+cd "$BASE_DIR/autoware_carla_interface"
+colcon build --symlink-install --packages-select autoware_carla_interface
+source "$BASE_DIR/autoware_carla_interface/install/setup.bash"
+
 cd "$BASE_DIR"
+
+echo "Build done."
+echo "Run these before launching:"
+echo "source $BASE_DIR/msg_ws/install/setup.bash"
+echo "source $BASE_DIR/bridge_ws/install/setup.bash"
+echo "source $BASE_DIR/autoware_carla_interface/install/setup.bash"
