dcd356b2f0398cdf46e14b091a217f7054525ef3
4a01004 <mangi2703@gmail.com>
2026-03-23 10:50:54 +0900

    fix: 그 bridge_ws를 업데이트 할때 bridges.launch.py까지 한번에 업데이트에서, control_cmd2가 제대로 나오도록함

--- bridge_install_script.sh
+++ bridge_install_script.sh
@@ -9,6 +9,8 @@ colcon build \
   --packages-select carla_autoware_type_bridge carla_autonomy_bridge \
   --cmake-clean-cache \
   --cmake-args \
+  --symlink-install \
+
   -Dautoware_auto_vehicle_msgs_DIR="$BASE_DIR/msg_ws/install/autoware_auto_vehicle_msgs/share/autoware_auto_vehicle_msgs/cmake"
 
 cd "$BASE_DIR/autoware_carla_interface"
